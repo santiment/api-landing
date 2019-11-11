eff01db975bf381cf28b977696ea4789ee1762a2
rfihtengolts <9-b-rinat@rambler.ru>
2019-11-11 19:42:21 +0300

    update header on mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,9 @@ import React, { useRef } from 'react'
 import cx from 'classnames'
 import Icon from '@santiment-network/ui/Icon'
 import { injectIntl, Link } from 'gatsby-plugin-intl'
-import styles from './Header.module.scss'
 import AccountBtn from '../AccountBtn/AccountBtn'
 import SantimentProductsTooltip from "../SantimentProductsTooltip/SantimentProductsTooltip"
+import styles from './Header.module.scss'
 
 const Header = ({ isAccountPage, intl }) => {
   const toggle = useRef(null)
@@ -28,11 +28,13 @@ const Header = ({ isAccountPage, intl }) => {
           <div className={styles.dividerLeft}/>
 
           <div className={styles.product}>
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10ZM13.3818 7.91355L14.0977 7.19891L13.353 6.45343C11.5014 4.60187 8.49938 4.60187 6.64782 6.45343L3.8456 9.25566C4.66756 10.078 6.00062 10.078 6.82299 9.25566L8.13631 7.94233C8.89947 7.17917 10.0175 6.98263 10.9632 7.35228L10.4044 7.91026L10.4048 7.91067C11.2264 8.73345 12.559 8.73469 13.3818 7.91355ZM13.353 13.5463L16.1548 10.7445C15.3324 9.92254 13.9994 9.92254 13.1778 10.7445L11.8645 12.0578C11.1013 12.821 9.98334 13.0175 9.03761 12.6479L9.59641 12.0903C8.77487 11.2671 7.44222 11.2659 6.61903 12.087L5.90274 12.8017L6.6474 13.5463C8.49937 15.3979 11.5014 15.3979 13.353 13.5463Z" fill="#5275FF"/>
-            </svg>
-            <div className={styles.productName}>API</div>
-            <SantimentProductsTooltip/>
+            <div className={styles.products}>
+              <SantimentProductsTooltip intl={intl}>
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" clipRule="evenodd" d="M20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10ZM13.3818 7.91355L14.0977 7.19891L13.353 6.45343C11.5014 4.60187 8.49938 4.60187 6.64782 6.45343L3.8456 9.25566C4.66756 10.078 6.00062 10.078 6.82299 9.25566L8.13631 7.94233C8.89947 7.17917 10.0175 6.98263 10.9632 7.35228L10.4044 7.91026L10.4048 7.91067C11.2264 8.73345 12.559 8.73469 13.3818 7.91355ZM13.353 13.5463L16.1548 10.7445C15.3324 9.92254 13.9994 9.92254 13.1778 10.7445L11.8645 12.0578C11.1013 12.821 9.98334 13.0175 9.03761 12.6479L9.59641 12.0903C8.77487 11.2671 7.44222 11.2659 6.61903 12.087L5.90274 12.8017L6.6474 13.5463C8.49937 15.3979 11.5014 15.3979 13.353 13.5463Z" fill="#5275FF"/>
+                </svg>
+                <div className={styles.productName}>API</div>
+              </SantimentProductsTooltip></div>
           </div>
 
         </Link>
@@ -47,17 +49,13 @@ const Header = ({ isAccountPage, intl }) => {
           <Link className={styles.link} to='/#use-cases' onClick={closeNav}>
             {intl.formatMessage({ id: 'header.use_cases' })}
           </Link>
-          <Link className={cx(styles.link, styles.linkLast)} to='/#pricing' onClick={closeNav}>
+          <Link className={styles.link} to='/#pricing' onClick={closeNav}>
             {intl.formatMessage({ id: 'header.pricing' })}
           </Link>
+          <Link className={cx(styles.link, styles.linkLast)} to='mailto:support@santiment.net' onClick={closeNav}>
+            {intl.formatMessage({ id: 'header.support' })}
+          </Link>
           <div className={styles.dividerRight}/>
-          <a
-            className={styles.headerIcon}
-            href='mailto:support@santiment.net'
-            onClick={closeNav}
-          >
-            <Icon type='question-round'/>
-          </a>
           <AccountBtn isAccountPage={isAccountPage} onClick={closeNav} />
         </nav>
       </div>
--- src/components/SantimentProductsTooltip/SantimentProductsTooltip.js
+++ src/components/SantimentProductsTooltip/SantimentProductsTooltip.js
@@ -11,8 +11,7 @@ const PRODUCTS = [
   {
     img: sanbaseLogoImg,
     title: 'Sanbase',
-    description:
-      'Behavior analysis & monitoring platform for 1000+ crypto assets',
+    description: 'header.product.sanbase',
     to: 'https://app.santiment.net'
   },
   {
@@ -29,12 +28,14 @@ const PRODUCTS = [
   }
 ]
 
-const ProductItem = ({ product: { to, img, title, description } }) => {
+const ProductItem = ({ product: { to, img, title, description }, intl }) => {
   const [showLink, setShowLink] = useState(false)
+
 
   return (
     <a
       href={to}
+      target='_blank' rel='noopener noreferrer'
       className={styles.product}
       onMouseEnter={() => setShowLink(true)}
       onMouseLeave={() => {
@@ -44,11 +45,12 @@ const ProductItem = ({ product: { to, img, title, description } }) => {
       <img className={styles.productImg} src={img} alt={title} />
       <div className={styles.productInfo}>
         <div className={styles.productTitle}>{title}</div>
-        <div className={styles.productDescription}>{description}</div>
+        <div className={styles.productDescription}> {intl.formatMessage({ id: description })}</div>
         {showLink && (
           <MakeLink
             className={styles.productLink}
             to={to}
+            as='div'
             title={'Go to ' + title}
           />
         )}
@@ -57,28 +59,29 @@ const ProductItem = ({ product: { to, img, title, description } }) => {
   )
 }
 
-const MakeLink = ({ to, title, className }) => (
-  <a href={to} className={cx(styles.link, className)}>
+const MakeLink = ({ to, title, className, as = 'a' }) => {
+  const El = as
+  return <El href={to} className={cx(styles.link, className)} >
     {title} <Icon className={styles.linkArrow} type='pointer-right' />
-  </a>
-)
+  </El>
+}
 
 const OpenTrigger = () => <Icon type='arrow-down' />
 const CloseTrigger = () => <Icon type='arrow-up' />
 
-const SantimentProductsTooltip = ({ className }) => {
+const SantimentProductsTooltip = ({ className, intl, children }) => {
   const [isOpen, setOpen] = useState(false)
 
   return (
     <Tooltip
-      passOpenStateAs='isActive'
       closeTimeout={150}
       position='bottom'
       align='start'
       offsetY={20}
       trigger={
-        <div className={cx(className, styles.arrow, isOpen && styles.opened)}>
-          {isOpen ? <CloseTrigger /> : <OpenTrigger />}
+        <div className={styles.trigger} >
+          {children}
+          <div className={cx(className, styles.arrow, isOpen && styles.opened)}>{isOpen ? <CloseTrigger /> : <OpenTrigger />}</div>
         </div>
       }
       onOpen={() => {
@@ -95,7 +98,7 @@ const SantimentProductsTooltip = ({ className }) => {
         </div>
         <div className={styles.products}>
           {PRODUCTS.map((item, index) => (
-            <ProductItem key={index} product={item} />
+            <ProductItem key={index} product={item} intl={intl} />
           ))}
         </div>
       </div>
